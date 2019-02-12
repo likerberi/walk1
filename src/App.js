3a50054bb37dddc8edc1102652a8d1afb45e9cbd
stepbystep <likerberi97@gmail.com>
2019-02-12 16:28:11 +0900

    init commit by create-react-app

--- src/App.js
+++ src/App.js
@@ -1,12 +1,11 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        
+      <div className="customBackground">
+        <h1>This is for checking git</h1>
       </div>
     );
   }
